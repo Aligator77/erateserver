63f79745ed719007e8cf5dda2485f4b3d7f52e08
Danil Zagoskin <stolen@yandex-team.ru>
2014-09-12 15:06:37 +0400

    fix badmatch in logging hook on bad request

--- src/erateserver_log.erl
+++ src/erateserver_log.erl
@@ -4,10 +4,15 @@
 -export([access/4]).
 
 access(Status, Headers, Body, Req) ->
-    {[{PeerAddr, _Port}, Method, Url], Req2} = lists:mapfoldl(fun get_req_prop/2, Req, [peer, method, url]),
+    {[Peer, Method, Url], Req2} = lists:mapfoldl(fun get_req_prop/2, Req, [peer, method, url]),
     {ok, ReqReplied} = cowboy_req:reply(Status, Headers, Body, Req2),
-    lager:info([{tag, access}, {rq_num, get(rq_num)}, {peer, inet_parse:ntoa(PeerAddr)}, {method, Method}, {url, Url}, {status, Status}], ""),
+    lager:info([{tag, access}, {rq_num, get(rq_num)}, {peer, peer_string(Peer)}, {method, Method}, {url, Url}, {status, Status}], ""),
     ReqReplied.
 
 get_req_prop(Prop, Req) ->
     cowboy_req:Prop(Req).
+
+peer_string({PeerAddr, _Port}) ->
+    inet_parse:ntoa(PeerAddr);
+peer_string(_) ->
+    "-".
